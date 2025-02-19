908f33f93ea2f8e2b982af5cf1b6fffe1df07025
julianficht <1126@htl.rennweg.at>
2025-02-19 15:23:19 +0100

    .

--- src/generate.php
+++ src/generate.php
@@ -3,13 +3,16 @@ require '../vendor/autoload.php';
 
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
-
-$qr = new QrCode('https://example.com');
+$phoneNumber = "tel:+4312233444";
+$qr = new QrCode($phoneNumber);
 $write = new PngWriter();
 
 try {
     $res = $write->write($qr);
 
-    $file = __DIR__ . '/qrcode.png';
+    $file = __DIR__ . '/tel.png';
     $res->saveToFile($file);
 } catch (\Exception $e){}
+
+header('Content-Type: '.$res->getMimeType());
+echo $res->getString();
---
+++ src/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tl</title>
+</head>
+<body>
+<h2>Tel +43 1 22 33 444</h2>
+<img src="generate.php" alt="QR Code for +43 1 22 33 444">
+</body>
+</html>
